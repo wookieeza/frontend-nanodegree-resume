4a355cf9ddc523bc7b394e74f75b08fe521f9bdb
Debbie Wood <wood.debbie@gmail.com>
2015-04-26 22:35:59 +0100

    work xp & edu to resume proj

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -23,6 +23,17 @@ var bio = {
     skills: ["java","javaScript","grails","Android","Swift"]
 };
 
+var work = {};
+work.position = "Engineer";
+work.employer =  "WhatClinic";
+work.yearsworked = 1;
+work.city = "Dublin";
+
+var education = {};
+education["name"] = "TCD";
+education["years"] = 2;
+education["city"] = "Dublin"
+
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 var formattedContactEmail = HTMLcontactGeneric.replace("%contact%","Email").replace("%data%", bio.contact.email);
@@ -30,6 +41,9 @@ var formattedContactTelephone = HTMLcontactGeneric.replace("%contact%","Mobile")
 var formattedHTMLbioPic = HTMLbioPic.replace("%data%", bio.pictureUrl);
 var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
 
+var formattedHTMLworkTitle = HTMLworkTitle.replace("%data%",work.position)
+
+var formattedHTMLschoolName = HTMLschoolName.replace("%data%",education["name"])
 
 $("#header").prepend([formattedName]);
 
@@ -41,5 +55,9 @@ $("#header").append([formattedHTMLwelcomeMsg]);
 $("#topContacts").append([formattedContactEmail]);
 $("#topContacts").append([formattedContactTelephone]);
 
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").append(formattedHTMLworkTitle);
+$("#education").append(HTMLschoolStart);
+$(".education-entry").append(formattedHTMLschoolName);
 
 
