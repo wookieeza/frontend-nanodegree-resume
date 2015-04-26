b23e8252b1bb50b49f762d77e0a7780a0fec2b55
Debbie Wood <wood.debbie@gmail.com>
2015-04-26 22:12:44 +0100

    start javascript basics course

---
+++ js/resumeBuilder.js
@@ -0,0 +1,45 @@
+//$("#main").append(['Debbie Wood']);
+
+var awesomeThoughts = "I am Debbie and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+console.log(funThoughts);
+
+//$("#main").append([funThoughts]);
+
+
+var audacityStr = "Audacity";
+var udacity = audacityStr.slice(1).replace("u","U");
+console.log(udacity);
+
+var bio = {
+    name: "Debbie Wood",
+    role: "Senior Engineer",
+    contact: { email: "debbie@test.com", telephone: "0879383576"},
+    pictureUrl: "images/fry.jpg",
+    welcomeMsg: "hello",
+    skills: ["java","javaScript","grails","Android","Swift"]
+};
+
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+var formattedContactEmail = HTMLcontactGeneric.replace("%contact%","Email").replace("%data%", bio.contact.email);
+var formattedContactTelephone = HTMLcontactGeneric.replace("%contact%","Mobile").replace("%data%", bio.contact.telephone);
+var formattedHTMLbioPic = HTMLbioPic.replace("%data%", bio.pictureUrl);
+var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+
+
+$("#header").prepend([formattedName]);
+
+$("#header").append([formattedRole]);
+
+
+$("#header").append([formattedHTMLbioPic]);
+$("#header").append([formattedHTMLwelcomeMsg]);
+$("#topContacts").append([formattedContactEmail]);
+$("#topContacts").append([formattedContactTelephone]);
+
+
+
